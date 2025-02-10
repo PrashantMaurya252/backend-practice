4cf4670cf476fdc42779a450836351b60680b506
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-02-10 22:56:01 +0530

    authentication

---
+++ authentication/controllers/auth-controller.js
@@ -0,0 +1,100 @@
+const User = require("../models/User.js");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// register endpoint
+
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password, role } = req.body;
+
+    // check if the user is already exist in our database
+
+    const checkExistingUser = await User.findOne({
+      $or: [{ username }, { email }],
+    });
+
+    if (checkExistingUser) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "User is already exists either with same username or same email ! Try different username or email",
+      });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      role: role || "user",
+    });
+    await newUser.save();
+
+    if (newUser) {
+      return res.status(201).json({
+        success: true,
+        message: "User registered successfully!",
+      });
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: "Unable to register user please try again",
+      });
+    }
+  } catch (error) {
+    console.log(error, "register error");
+    res.status(500).json({
+      success: false,
+      message: "Some error occured! Please try again",
+    });
+  }
+};
+
+// login controller
+
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const userExist = await User.findOne({ username });
+    if (!userExist) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, userExist.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+    const accessToken = jwt.sign(
+      {
+        userId: userExist._id,
+        username: userExist.username,
+        role: userExist.role,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "15m" }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Logged in successful",
+      accessToken,
+    });
+  } catch (error) {
+    console.log(error, "register error");
+    res.status(500).json({
+      success: false,
+      message: "Some error occured! Please try again",
+    });
+  }
+};
+
+module.exports = { loginUser, registerUser };
--- authentication/server.js
+++ authentication/server.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
 const connectToDb = require('./database/db.js')
+const authRoutes = require('./routes/auth-routes.js')
+const homeRoutes = require('./routes/home-routes.js')
+const adminRoutes = require('./routes/admin-routes.js')
 
 const express = require('express')
 const app =express()
@@ -7,7 +10,14 @@ const app =express()
 const PORT = process.env.PORT || 3000
 
 // connect to db
+
+
 connectToDb()
+
+app.use(express.json())
+app.use('/api/auth',authRoutes)
+app.use('/api/home',homeRoutes)
+app.use('/api/admin',adminRoutes)
 app.listen(PORT,()=>{
     console.log(`Server is listening at ${PORT}`)
 })
