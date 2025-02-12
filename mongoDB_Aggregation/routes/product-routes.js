f8f4e5b3d545ae2cd24d7b6b9a479f18ac18aaf1
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-02-12 13:44:17 +0530

    mongoDb aggregation

---
+++ mongoDB_Aggregation/routes/product-routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const {insertSampleProducts} = require('../controllers/product-controller')
+const router = express.Router()
+
+router.post('/add',insertSampleProducts)
+
+module.exports = router
---
+++ mongoDB_Aggregation/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const productRoutes = require('./routes/product-routes')
+const app = express()
+
+// connect to our database
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log(`mongoDB connected successfully`)
+}).catch((error)=>console.log(error,"mongoDb connecttion error"))
+
+// use middlewares
+app.use(express.json());
+app.use("/products",productRoutes)
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is now running on port ${process.env.PORT}`)
+})
