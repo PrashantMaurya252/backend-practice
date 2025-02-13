d64a01b974671a0ddb257a46ff90ee4bd83a2493
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-02-13 22:38:52 +0530

    graphql basics

---
+++ graphql/src/graphql/schema.js
@@ -0,0 +1,12 @@
+// this file will tell that what will be structure of your data
+
+const {gql} = require('graphql-tag')
+// String
+// Int
+// Float
+// Boolean
+// ID -> an unique identifier
+const typeDefs = gql`
+type Product {
+
+}`
